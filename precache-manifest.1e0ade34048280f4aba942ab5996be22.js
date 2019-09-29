e6a706f322c2990479aac98722908c0be3a12e27
Jon (as Circle Bot) <berkona@gmail.com>
2019-09-29 21:10:56 +0000

    Deploy to GitHub pages [ci skip]

---
+++ precache-manifest.1e0ade34048280f4aba942ab5996be22.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309435de5c241918397731e098b241bf",
+    "url": "/project-fenris/index.html"
+  },
+  {
+    "revision": "8a86e2cd9d14974a3ed9",
+    "url": "/project-fenris/static/js/2.9bed3434.chunk.js"
+  },
+  {
+    "revision": "235bea9343a7777cf7c3",
+    "url": "/project-fenris/static/js/main.f33df012.chunk.js"
+  },
+  {
+    "revision": "3380d2e98065b63c1316",
+    "url": "/project-fenris/static/js/runtime-main.d6053c7c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.694c38fef1f5a7d45a604a5d968727e0.js"
+  "/project-fenris/precache-manifest.1e0ade34048280f4aba942ab5996be22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-fenris/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
